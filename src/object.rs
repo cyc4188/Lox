892746997199efd339763b2bd56bc412deefe262
cyc4188 <1065768794@qq.com>
2023-06-21 17:54:21 +0800

    wip: NumberType

--- src/object.rs
+++ src/object.rs
@@ -1,10 +1,14 @@
+use std::cell::RefCell;
 use std::fmt::Display;
 use std::rc::Rc;
-use std::cell::RefCell;
 
+use crate::Error;
+use crate::ErrorType;
 use crate::Function;
 use crate::LoxClass;
 use crate::LoxInstance;
+use crate::Token;
+use crate::TokenType;
 type ClassRef = Rc<RefCell<LoxClass>>;
 type InstanceRef = Rc<RefCell<LoxInstance>>;
 
@@ -42,5 +46,89 @@ impl Object {
             (Object::Nil, Object::Nil) => true,
             _ => false,
         }
+    }
+}
+
+pub enum NumberType {
+    Integer(i64),
+    Float(f64),
+}
+
+impl NumberType {
+    pub fn as_float(&self) -> f64 {
+        match self {
+            NumberType::Integer(i) => *i as f64,
+            NumberType::Float(f) => *f,
+        }
+    }
+    pub fn binary_op(&self, op: Token, other: &NumberType) -> Result<Self, Error> {
+        match op.token_type {
+            _ => Err(Error {
+                message: format!("Unsupported binary operation",),
+                error_type: ErrorType::RuntimeError(op.clone()),
+            }),
+        }
+    }
+    pub fn add(&self, other: &NumberType) -> Result<Self, Error> {
+        use NumberType::{Float, Integer};
+        Ok(match self {
+            Integer(i) => match other {
+                Integer(j) => Integer(i + j),
+                Float(f) => Float(*i as f64 + f),
+            },
+            Float(f) => match other {
+                Integer(i) => Float(f + *i as f64),
+                Float(g) => Float(f + g),
+            },
+        })
+    }
+    pub fn sub(&self, other: &NumberType) -> Result<Self, Error> {
+        use NumberType::{Float, Integer};
+        Ok(match self {
+            Integer(i) => match other {
+                Integer(j) => Integer(i - j),
+                Float(f) => Float(*i as f64 - f),
+            },
+            Float(f) => match other {
+                Integer(i) => Float(f - *i as f64),
+                Float(g) => Float(f - g),
+            },
+        })
+    }
+
+    pub fn mul(&self, other: &NumberType) -> Result<Self, Error> {
+        use NumberType::{Float, Integer};
+        Ok(match self {
+            Integer(i) => match other {
+                Integer(j) => Integer(i * j),
+                Float(f) => Float(*i as f64 * f),
+            },
+            Float(f) => match other {
+                Integer(i) => Float(f * *i as f64),
+                Float(g) => Float(f * g),
+            },
+        })
+    }
+    pub fn div(&self, other: &NumberType) -> Result<Self, Error> {
+        use NumberType::{Float, Integer};
+        let result = match self {
+            Integer(i) => match other {
+                Integer(j) => {
+                    if j == &0 {
+                        return Err(Error {
+                            message: String::from("Division by zero."),
+                            error_type: ErrorType::SyntaxError,
+                        });
+                    }
+                    Integer(i / j)
+                }
+                Float(f) => Float(*i as f64 / f),
+            },
+            Float(f) => match other {
+                Integer(i) => Float(f / *i as f64),
+                Float(g) => Float(f / g),
+            },
+        };
+        Ok(result)
     }
 }
